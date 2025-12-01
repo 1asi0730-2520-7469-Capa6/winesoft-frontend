d347dacd4331f88e121d29a4d531fd12e8ed524d
Teffo-Code <145604140+StephanoDang@users.noreply.github.com>
2025-11-30 19:03:55 -0500

    fix(router/main/i18n): resolve blank page by sanitizing locales JSON, fixing PrimeVue imports and router routes.

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import Home from "./shared/presentation/views/home.vue";
-import SignUp from "./identity/presentation/views/sign-in.vue";
-import SignIn from "./identity/presentation/views/sign-up.vue";
+import SignUp from "./identity/presentation/views/sign-up.vue";
+import SignIn from "./identity/presentation/views/sign-in.vue";
 import {createRouter, createWebHistory} from "vue-router";
 import inventoryRoutes from "./inventory/presentation/inventory.routes.js";
 import analyticsRoutes from "./analytics/presentation/analytics.routes.js";
 import purchaseRoutes from "./purchase/presentation/purchase.routes.js";
+import ordersRoutes from "./orders/presentation/orders.routes.js";
 
 const pageNotFound = () => import('./shared/presentation/views/page-not-found.vue');
 
@@ -16,10 +17,10 @@ const routes = [
     { path: '/recovery-password', name: 'recovery-password', component: () => import('./identity/presentation/views/recovery-password.vue'), meta: { title: 'Recuperar Contraseña' } },
     { path: '/inventory',      name: 'inventory', children: inventoryRoutes },
     { path: '/orders',         name: 'orders',    children: ordersRoutes },
-    { path: '/',                redirect: '/sign-in'},
-    { path: '/:pathMatch(.*)*', name: 'not-found',  component: pageNotFound,    meta: { title: 'Page Not Found' } },
     { path: '/analytics', name: 'analytics-main', children: analyticsRoutes },
     { path: '/purchase/orders', name: 'purchase', children: purchaseRoutes },
+    { path: '/',                redirect: '/sign-in'},
+    { path: '/:pathMatch(.*)*', name: 'not-found',  component: pageNotFound,    meta: { title: 'Page Not Found' } }
 ];
 
 const router = createRouter({
